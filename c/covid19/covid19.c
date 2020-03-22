ba71d23b36aa3f3a125501338b358ff85a5db01b
Ismael Damião <53145011+ismaeldamiao@users.noreply.github.com>
2020-03-21 22:55:17 -0300

    Update covid19.c

--- c/covid19/covid19.c
+++ c/covid19/covid19.c
@@ -78,23 +78,23 @@ int main(void){
    Valores comumente referentes ah pandemia de COVID-19 ao redor do mundo
    -------------------------------------------------------------------------- */
    gamma = 0.04;
-   mu = 0.005; // Na Italia: mu = 0.04;
+   mu = 1.0e-8; // Na Italia: mu = 0.04;
    /* --------------------------------------------------------------------------
    Valores locais, isto eh, do lugar de onde se deseja fazer a simulacao
    -------------------------------------------------------------------------- */
-   sprintf(sitio, "Alagoas");
-   N = 993733.0;//3337357.0; // Populacao local em numeros absolutos
-   I = 6.0;// Eh preciso informar a populacao infectada no inicio da iteracao
-   R0 = 3.0; // Informe o Ritmo basico de reproducao
-   Tmax = 80.0; // Observar ate 150 dias
+   sprintf(sitio, "Maceio");
+   N = 1012382.0; // Populacao local em numeros absolutos
+   I = 7.0;// Eh preciso informar a populacao infectada no inicio da iteracao
+   R0 = 5.2; // Informe o Ritmo basico de reproducao
+   Tmax = 150.0; // Observar ate 150 dias
    beta = R0 * gamma;
    //beta = 0.38;
    I = I/N;
    /* ----------------------------------------------------------------------- */
    lim = (int)(1.0e-4/dt);
    sprintf(plotSetS, "with linespoints lc rgb 'blue' pt 7 ps .3 title 'suscetíveis'");
-   sprintf(plotSetI, "with linespoints lc rgb 'brown' pt 7 ps .3 title 'Infectados'");
-   sprintf(plotSetC, "with linespoints lc rgb 'black' pt 7 ps .3 title 'Curados'");
+   sprintf(plotSetI, "with linespoints lc rgb 'black' pt 7 ps .3 title 'Infectados'");
+   sprintf(plotSetC, "with linespoints lc rgb 'brown' pt 7 ps .3 title 'Curados'");
    sprintf(plotSetM, "with linespoints lc rgb 'red' pt 7 ps .3 title 'Mortos'");
 
    sprintf(NS, "S.dat");
@@ -122,10 +122,13 @@ int main(void){
 
       ++count;
       if(count > lim){
-         fprintf(fS, "%g %g\n", t, S * 100.0);
-         fprintf(fI, "%g %g\n", t, I * 100.0);
-         fprintf(fC, "%g %g\n", t, C * 100.0);
-         fprintf(fM, "%g %g\n", t, M * 100.0);
+         //fprintf(fS, "%g %g\n", t, S * 100.0);
+         //fprintf(fI, "%g %g\n", t, I * 100.0);
+         //fprintf(fC, "%g %g\n", t, C * 100.0);
+         //fprintf(fM, "%g %g\n", t, M * 100.0);
+         //fprintf(fS, "%g %g\n", t, S * N);
+         fprintf(fI, "%g %g\n", t, I * N);
+         //fprintf(fC, "%g %g\n", t, C * N);
          //fprintf(fM, "%g %g\n", t, M * N);
          count = 0;
       }
@@ -135,17 +138,20 @@ int main(void){
    fclose(fI);
    fclose(fC);
    fclose(fM);
-   gnuplotOpen("%s_gamma%g_beta%g.png", sitio, gamma, beta);
+   //gnuplotOpen("%s_gamma%g_beta%g.png", sitio, gamma, beta);
+   gnuplotOpen("%s.png", sitio);
    gnuplotPrintf("set xlabel 'Tempo (dias)'");
    gnuplotPrintf("set ylabel 'População (%%)'");
    gnuplotPrintf("set title '%s,  γ=%g, β=%g'", sitio, gamma, beta);
-   gnuplotPrintf("plot '%s' %s, '%s' %s, '%s' %s, '%s' %s",
-      NS, plotSetS, NI, plotSetI, NC, plotSetC, NM, plotSetM);
+   //gnuplotPrintf("plot '%s' %s, '%s' %s, '%s' %s, '%s' %s",
+   //   NS, plotSetS, NI, plotSetI, NC, plotSetC, NM, plotSetM);
+   //gnuplotPrintf("plot '%s' %s, 'confirmados.dat' with linespoints lc rgb 'red' pt 7 ps .3 title 'Confirmados'", NI, plotSetI);
+   gnuplotPrintf("plot '%s' %s", NI, plotSetI);
    gnuplotClose();
    sprintf(cmd, "rm %s %s %s %s", NS, NI, NC, NM);
-   system(cmd);
+   //system(cmd);
 
-   printf("Taxa de infecção β: %2.0lf%%\n", S * 100.0);
+   printf("Taxa de infecção β: %2.2lf\n", beta);
    printf("Porcentagem de pessoas que evitaram a infecção: %2.0lf%%\n", S * 100.0);
    printf("Porcentagem de pessoas infectadas: %2.0lf%%\n", I * 100.0);
    printf("Porcentagem de pessoas removidas: %2.0lf%%\n\n", R * 100.0);
