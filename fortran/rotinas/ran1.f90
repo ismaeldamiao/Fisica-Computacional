ebd1a676e8c4cc83c60a08c74f351d25cffa3840
Ismael Damião <53145011+ismaeldamiao@users.noreply.github.com>
2020-09-03 15:01:00 -0300

    Update ran1.f90

--- fortran/rotinas/ran1.f90
+++ fortran/rotinas/ran1.f90
@@ -1,9 +1,22 @@
+! Gerador de numeros aleatorios.
+! Adaptado ao Fortran 90 por SANTOS. I.F.F. (Ismael Damião)
+! https://ismaeldamiao.github.io/
+! "Numerical Recipes in Fortran 77" (Cambridge Univ. Press)
+! The Art of Scientific Computing, 2nd Edition, 1992 p 280
+! by W.H. Press, S.A. Teukolsky, W.T. Vetterling, and B.P. Flannery
+
+! Nao usar para gerar mais de 100000000 de numeros, use ran2 ou ran3
+! double var = ran1(&semente); // Guarda em var um numero aleatorio em [0, 1)
 MODULE ran
-! Subrotina para preencher a matriz
 
 CONTAINS
-
-   FUNCTION ran1(idum)
+! “Minimal” random number generator of Park and Miller with Bays-Durham shuffle
+! and added safeguards. Returns a uniform random deviate between 0.0 and 1.0
+! (exclusive of the endpoint values). Call with idum a negative integer to
+! initialize; thereafter, do not alter idum between successive deviates in a
+! sequence. RNMX should approximate the largest floating value that is less
+! than 1.
+   REAL(8) FUNCTION ran1(idum)
       INTEGER(8), PARAMETER :: IA = 16807, IM = 2147483647, IQ = 127773,&
       NTAB = 32, NDIV = 1+(IM-1)/NTAB, IR = 2836
       REAL(8), PARAMETER :: AM = 1.0d0/IM, EPS = 1.2d-7, RNMX = 1.0d0-EPS
