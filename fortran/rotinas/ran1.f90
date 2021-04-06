1fcf8eb05b6fb5ef6fd092d3ad85b2b134b4e105
Ismael Damião <53145011+ismaeldamiao@users.noreply.github.com>
2021-04-06 04:15:24 -0300

    Update ran1.f90

--- fortran/rotinas/ran1.f90
+++ fortran/rotinas/ran1.f90
@@ -7,40 +7,37 @@
 
 ! Nao usar para gerar mais de 100000000 de numeros, use ran2 ou ran3
 ! double var = ran1(&semente); // Guarda em var um numero aleatorio em [0, 1)
-MODULE ran
 
-CONTAINS
 ! “Minimal” random number generator of Park and Miller with Bays-Durham shuffle
 ! and added safeguards. Returns a uniform random deviate between 0.0 and 1.0
 ! (exclusive of the endpoint values). Call with idum a negative integer to
 ! initialize; thereafter, do not alter idum between successive deviates in a
 ! sequence. RNMX should approximate the largest floating value that is less
 ! than 1.
-   REAL(8) FUNCTION ran1(idum)
-      INTEGER(8), PARAMETER :: IA = 16807, IM = 2147483647, IQ = 127773,&
-      NTAB = 32, NDIV = 1+(IM-1)/NTAB, IR = 2836
-      REAL(8), PARAMETER :: AM = 1.0d0/IM, EPS = 1.2d-7, RNMX = 1.0d0-EPS
-      INTEGER(8), intent(inout) :: idum
-      INTEGER(8) :: j, k
-      INTEGER(8), SAVE :: iy = 0, iv(NTAB)
-      
-      IF((idum.le.0) .or. (iy.eq.0)) THEN
-         idum = MAX(-idum, 1)
-         DO j = NTAB+8, 1, -1
-            k = idum / IQ
-            idum = IA*(idum-k*IQ)-IR*k
-            IF(idum .lt. 0) idum = idum + IM
-            IF(j .le. NTAB) iv(j) = idum
-         ENDDO
-         iy=iv(1)
-      ENDIF
-      k = idum/IQ
-      idum = IA*(idum-k*IQ)-IR*k
-      IF(idum.lt.0) idum = idum+IM
-      j = 1 + iy/NDIV
-      iy = iv(j)
-      iv(j) = idum
-      ran1 = MIN(AM*iy, RNMX)
-      return
-   END FUNCTION ran1
-END MODULE ran
+REAL(8) FUNCTION ran1(idum)
+   INTEGER(8), PARAMETER :: IA = 16807, IM = 2147483647, IQ = 127773,&
+   NTAB = 32, NDIV = 1+(IM-1)/NTAB, IR = 2836
+   REAL(8), PARAMETER :: AM = 1.0d0/IM, EPS = 1.2d-7, RNMX = 1.0d0-EPS
+   INTEGER(8), intent(inout) :: idum
+   INTEGER(8) :: j, k
+   INTEGER(8), SAVE :: iy = 0, iv(NTAB)
+
+   IF((idum.le.0) .or. (iy.eq.0)) THEN
+      idum = MAX(-idum, 1)
+      DO j = NTAB+8, 1, -1
+         k = idum / IQ
+         idum = IA*(idum-k*IQ)-IR*k
+         IF(idum .lt. 0) idum = idum + IM
+         IF(j .le. NTAB) iv(j) = idum
+      ENDDO
+      iy=iv(1)
+   ENDIF
+   k = idum/IQ
+   idum = IA*(idum-k*IQ)-IR*k
+   IF(idum.lt.0) idum = idum+IM
+   j = 1 + iy/NDIV
+   iy = iv(j)
+   iv(j) = idum
+   ran1 = MIN(AM*iy, RNMX)
+   return
+END FUNCTION ran1
